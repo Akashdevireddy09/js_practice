6b587e2c82dbbd5a696ec5d6f5c4ec0e8fdd3e9c
Akash Devireddy <105554228+Akashdevireddy09@users.noreply.github.com>
2025-07-09 15:06:37 +0000

    variables and dataTypes

---
+++ basics/dataTypes.js
@@ -0,0 +1,8 @@
+"use strict" // treat all JS code as newer version
+
+// alert("Hello.. Akash") //we are using nodejs,not browser
+console.log(3
+    +3) //code readability should be consider
+
+
+console.log("Akash Devireddy")
---
+++ basics/variables.js
@@ -0,0 +1,8 @@
+const dob="18/09/2002";
+let age=22;
+var name="Akash Devireddy"
+email="devireddyakash0988@gmail.com"
+
+console.log(email)
+
+console.table([name,dob,age,email]);
