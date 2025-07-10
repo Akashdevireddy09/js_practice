4c369757c2ea69e500b598ea9f4a21be2f7d0478
Akash Devireddy <105554228+Akashdevireddy09@users.noreply.github.com>
2025-07-10 08:36:36 +0000

    operations

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -2,27 +2,27 @@ let score="33"
 // typeof score or typeof(score)
 
 let valueInNumber =Number(score)
-console.log(typeof(valueInNumber));
+// console.log(typeof(valueInNumber));
 
 let score2="33abc";
 let valueInNumber2 =Number(score2)
-console.log(typeof(valueInNumber2));
-console.log(valueInNumber2);
+// console.log(typeof(valueInNumber2));
+// console.log(valueInNumber2);
 
 let score3=null;
 let valueInNumber3 =Number(score3)
-console.log(typeof(valueInNumber3));
-console.log(valueInNumber3);
+// console.log(typeof(valueInNumber3));
+// console.log(valueInNumber3);
 
 let score4=undefined;
 let valueInNumber4 =Number(score4)
-console.log(typeof(valueInNumber4));
-console.log(valueInNumber4);
+// console.log(typeof(valueInNumber4));
+// console.log(valueInNumber4);
 
 let score5=true;
 let valueInNumber5 =Number(score5)
-console.log(typeof(valueInNumber5));
-console.log(valueInNumber5);
+// console.log(typeof(valueInNumber5));
+// console.log(valueInNumber5);
 
 //"33" --> 33
 //"33abs" --> NaN
@@ -32,8 +32,8 @@ let flag=1;
 let flag2=34;
 let boolean=Boolean(flag);
 let boolean2=Boolean(flag2);
-console.log(boolean);
-console.log(boolean2);
+// console.log(boolean);
+// console.log(boolean2);
 
 
 //"akash"--> true;
@@ -41,6 +41,41 @@ console.log(boolean2);
 //""--> false
 let n=33;
 let s=String(n);//"String()" to convert into String.
-console.log(typeof(s));
+// console.log(typeof(s));
+
+//***********Operations***** *//
+
+let value=3;
+let negvalue= -value;
+// console.log(negvalue);
+
+// console.log(5+5);
+// console.log(5-5);
+// console.log(5*5);
+// console.log(5**5);
+// console.log(10/5);
+// console.log(10%5);
+
+let str1='Akash'
+let str2=" Devireddy"
+// console.log(str1+str2);
+
+// console.log("1"+2);//12
+// console.log(1+"2");//12
+// console.log(1+1+"2");//22
+// console.log("1"+"2");//12
+// console.log(2+5+"7"+"5"+"4");//7754
+
+console.log(+true);
+console.log(+"");
+
+
+
+
+
+
+
+
+
 
 
